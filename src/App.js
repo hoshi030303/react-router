d84c5aa9be3987966949cecfa97335c5df97dc50
hoshi030303 <68181024+hoshi030303@users.noreply.github.com>
2021-07-18 11:01:59 +0900

    routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 
-import { Home } from "./Home";
-import { Page1 } from "./Page1";
-import { Page2 } from "./Page2";
+import { Router } from "./router/Router";
 import "./styles.css";
 
 export default function App() {
@@ -14,21 +12,8 @@ export default function App() {
         <Link to="/page1">Page1</Link>
         <br />
         <Link to="/page2">Page2</Link>
-        <Home />
-        <Page1 />
-        <Page2 />
       </div>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/page1">
-          <Page1 />
-        </Route>
-        <Route path="/page2">
-          <Page2 />
-        </Route>
-      </Switch>
+      <Router />
     </BrowserRouter>
   );
 }
